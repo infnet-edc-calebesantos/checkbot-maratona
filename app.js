fd028a2e33a7054f9267e161b7b694421bf72ffb
Calebe Santos <calebe.bsantos@outlook.com>
2018-03-18 01:34:14 -0300

    :lipstick: Change messagem reproduced

--- app.js
+++ app.js
@@ -37,5 +37,5 @@ var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
 
 bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
+    session.send('You said: "' + session.message.text + '"');
 });
